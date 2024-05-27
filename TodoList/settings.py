5720931ad5b8850acceb6f8d728dd9d2bf377c45
Kenneth <hadoken10@yahoo.com>
2024-05-27 16:49:06 +0100

    include local host in settings.py file

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["django-render-todolist-app.onrender.com"]
 
 
 # Application definition
